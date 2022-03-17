677ac796a8f0ff0269c417217ab2fd02b9d59d27
czarnaherbata <czarnaherbata223@gmail.com>
2022-03-17 17:38:13 +0100

    fence rail description

--- szyfrowanie_l/src/main/java/Encrypt.java
+++ szyfrowanie_l/src/main/java/Encrypt.java
@@ -8,8 +8,9 @@ public class Encrypt {
         String word=s.nextLine();
         System.out.println("Wprowadz kod:");
         int key=s.nextInt();
-        //  EncryptFence(word, key);
-        DescFence(word, key);
+          EncryptFence(word, key);
+       // DescFence(word, key);
+        
     }
 
     static String EncryptFence(String word, int key)
@@ -64,58 +65,94 @@ public class Encrypt {
     static String DescFence(String word, int key)
     {
 //
-//        String descryptWord="";
-//        int position=0;
-//        boolean directionDown=false;// czy poruszamy sie w dol czy w gore
-//        int x=key; //ilosc wierszy
-//        int y=word.length(); //ilosc kolumn
-//
-//        char[][] fence=new char[x][y];
-//        for(int i=0; i<y;i++)
+        String descryptWord="";
+        int position=0;
+        boolean directionDown=false;// czy poruszamy sie w dol czy w gore
+        int x=key; //ilosc wierszy
+        int y=word.length(); //ilosc kolumn
+
+        char[][] fence=new char[x][y]; //tablica na ktorej szygrujemy
+
+        for(int i=0; i<x; i++)
+        {
+            for (int j=0; j<y;j++)
+            {
+                fence[i][j]='0'; //poczatkowo wypelniam tablice 0 bo tak mi wygodniej
+
+            }
+            System.out.println();
+        }
+
+        for(int i=0; i<y;i++)
+        {
+            if(position==0 || position==key-1) //jesli jestem w pierzym lub ostanim rzecie macierzy
+            {
+                directionDown=!directionDown;//zmieniam pozycje z gory na dol albo odwrotnie
+            }
+
+            fence[position][i]='*';//wpisuje sobie znaczki
+
+            if(directionDown)//jak ide w dol to numer rzedu rosnie
+            {
+                position++;
+            }
+            else //a jak do gory to maleje
+            {
+                position--;
+            }
+        }
+       // position=0;
+       // directionDown=false;
+//        for(int i=0; i<x;i++)
 //        {
-//            if(position==0 || position==key-1) //jesli jestem w pierzym lub ostanim rzecie macierzy
+//            for (int j=0; j<y; j++)
 //            {
-//                directionDown=!directionDown;//zmieniam pozycje z gory na dol albo odwrotnie
+//                System.out.print(fence[i][j]);
 //            }
-//
-//            fence[position][i]='0';//wpisuje sobie znaczki
-//
-//            if(directionDown)//jak ide w dol to numer rzedu rosnie
-//            {
-//                position++;
-//            }
-//            else //a jak do gory to maleje
-//            {
-//                position--;
-//            }
+//            System.out.println(" ");
 //        }
-//        int p=0;
-//        for(int i=0;i<x; i++)
-//        {
-//            for(int j=0;j<x; j++)
-//            {
-//                if(fence[i][j]=='0' && p<y)
-//                {
-//                    fence[i][j]=word.charAt(p++);
-//                }
-//            }
-//        }
+        int p=0;
+        for(int i=0; i<x; i++)
+        {
+            for(int j=0;j<y;j++)
+                if(fence[i][j]=='*' && p<y )
+                {
+                    fence[i][j]=word.charAt(p++);
+
+                }
+        }
 //        for(int i=0; i<x; i++)
 //        {
-//            for (int j=0; j<y;j++)
+//            for(int j=0; j<y;j++)
 //            {
-//              //  if(fence[i][j]!='0')
-//                    System.out.print(fence[i][j] + " ");
-//
+//                System.out.print(fence[i][j]);
 //            }
-//            System.out.println( " ");
+//
+//            System.out.println(" ");
 //        }
-//
-//
-//
-//
-//        return descryptWord;
-        return null;
+        directionDown=false;
+        position=0;
+        for(int i=0; i<y;i++)
+        {
+            if(position==0 || position==key-1) //jesli jestem w pierzym lub ostanim rzecie macierzy
+            {
+                directionDown=!directionDown;//zmieniam pozycje z gory na dol albo odwrotnie
+            }
+
+            descryptWord+=fence[position][i];//wpisuje sobie znaczki
+
+            if(directionDown)//jak ide w dol to numer rzedu rosnie
+            {
+                position++;
+            }
+            else //a jak do gory to maleje
+            {
+                position--;
+            }
+        }
+    System.out.print(descryptWord);
+        return descryptWord;
+        //return null;
     }
 
 }
