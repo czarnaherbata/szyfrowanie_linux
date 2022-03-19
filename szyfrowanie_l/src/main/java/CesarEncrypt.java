1e23fc2426bae0849282346606f938579a7f6eb2
czarnaherbata <czarnaherbata223@gmail.com>
2022-03-19 22:02:21 +0100

    CesarEncrypt

---
+++ szyfrowanie_l/src/main/java/CesarEncrypt.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class CesarEncrypt {
+    static String EncryptCesar(String word, int key)
+    {
+        Character[] alphabetLow = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        Character[] alphabetUp = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        char character;
+        String encryptedWord=" ";
+        char[] tab=new char[word.length()];
+        int k=0;
+
+
+        for(int i=0; i<word.length();i++)
+        {
+            for(int j=0; j<alphabetLow.length;j++)
+            {
+                if(word.charAt(i)==alphabetLow[j])
+                {
+                    encryptedWord+=alphabetLow[(j+key)%26];
+                }
+                if(word.charAt(i)==alphabetUp[j])
+                {
+                    encryptedWord+=alphabetUp[(j+key)%26];
+                }
+            }
+        }
+//
+        System.out.println(encryptedWord);
+        return encryptedWord;
+    }
+}
--- szyfrowanie_l/src/main/java/Main.java
+++ szyfrowanie_l/src/main/java/Main.java
@@ -3,33 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-//        Scanner s=new Scanner(System.in);
-//        System.out.println("Wprowadz slowo: ");
-//        String word=s.nextLine();
-//        System.out.println("Wprowadz kod:");
-//        int key1=s.nextInt();
-//       // int key2 =s.nextInt();
-//        FenceEncrypt.EncryptFence(word, key1);
-//       // FenceDesc.DescFence(word, key1);
         Scanner s=new Scanner(System.in);
         System.out.println("Wprowadz slowo: ");
         String word=s.nextLine();
-        System.out.println("Wpisz dlugosc klucza:");
-        int lenght=s.nextInt();
-        int key[]=new int[lenght];
+        System.out.println("Wprowadz kod:");
+        int key=s.nextInt();
 
-        System.out.println("Wpisz klucz:");
-        for(int i=0; i<lenght;i++)
-        {
 
-               int k=s.nextInt();
-               key[i]=k;
-
-        }
-        for(int i=0;i<lenght;i++)
-        System.out.println(key[i]);
-       // MatrixEnsctypt.EncryptMatrix(word, key);
-        MatrixDescrypt.DescMatrix(word,key);
+        CesarEncrypt.EncryptCesar(word,key);
+       // int key2 =s.nextInt();
+       // FenceEncrypt.EncryptFence(word, key);
+       // FenceDesc.DescFence(word, key1);
+//        Scanner s=new Scanner(System.in);
+//        System.out.println("Wprowadz slowo: ");
+//        String word=s.nextLine();
+//        System.out.println("Wpisz dlugosc klucza:");
+//        int lenght=s.nextInt();
+//        int key[]=new int[lenght];
+//
+//        System.out.println("Wpisz klucz:");
+//        for(int i=0; i<lenght;i++)
+//        {
+//
+//               int k=s.nextInt();
+//               key[i]=k;
+//
+//        }
+//        for(int i=0;i<lenght;i++)
+//        System.out.println(key[i]);
+//       // MatrixEnsctypt.EncryptMatrix(word, key);
+       // MatrixDescrypt.DescMatrix(word,key);
 
 
     }
